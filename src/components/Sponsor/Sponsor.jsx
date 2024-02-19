4c43b50a3c2569dcc5f86d769fd22ccdca8ee3e1
Rihansh <rihanshhingad113@gmail.com>
2024-02-19 23:15:19 +0530

    Sponsors

--- src/components/Sponsor/Sponsor.jsx
+++ src/components/Sponsor/Sponsor.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import "./Sponsor.css"
-import comingSoon from "../../assets/images/comingSoon.png"
+import React from 'react';
+import "./Sponsor.css";
+import eth from "../../assets/images/Devfolio_Logo-White@2x.png";
+import Devfolio from "../../assets/images/ethindia-light.png";
+import Polygon from "../../assets/images/Polygon_Logo-Colored@2x.png";
 
 const Sponsor = () => {
   return (
     <>
-     <div className='sponsor-container'>
-        <h1 className='sponsor-Head'>
-            Sponsors
+      <div className='sponsor-container  '>
+        <h1 className='sponsor-Head justify-center text-center items-center flex flex-col '>
+          Sponsors
         </h1>
-        <img className='sponsor-img' src={comingSoon} alt='comingSoon'/>
-        </div> 
+        <div className="flex mt-8">
+          <img className='sponsor-img mr-4' src={Devfolio} alt='Devfolio'/>
+          <img className='sponsor-img ml-[8rem]' src={Polygon} alt='Polygon'/>
+        </div>
+        <img className='sponsor-img mt-4' src={eth} alt='Eth'/>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Sponsor
+export default Sponsor;
